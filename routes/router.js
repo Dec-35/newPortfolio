39a405788a52475992709bf361b3c2b45a6cd471
dec <decleggal@gmail.com>
2024-02-22 08:40:52 +0100

    added projects templates

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,8 @@
 //router file
 const express = require('express');
+const file = require('js-yaml');
+const fs = require('fs');
+const {log} = require('console');
 
 const router = express.Router();
 
@@ -9,6 +12,22 @@ router.get('/', (req, res) => {
 
 router.get('/about', (req, res) => {
   res.send('About Page');
+});
+
+router.get('/project/:id', (req, res) => {
+  const {id} = req.params;
+
+  //search for project by id using the projects.yml file
+  let projects = file.load(fs.readFileSync('./projects.yml', 'utf8'));
+  projects = projects.projects;
+
+  const project = projects.find(
+    (project) =>
+      project.name.replace(' ', '-').toLowerCase() === id.toLowerCase()
+  );
+  if (!project) return res.status(404).send('Project not found');
+
+  res.render('project', {project});
 });
 
 module.exports = router;
