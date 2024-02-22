06f5c0d2322dfc78bcaeeb10091c098df6788743
dec <decleggal@gmail.com>
2024-02-22 08:22:26 +0100

    added absolute path to views

--- server.js
+++ server.js
@@ -5,13 +5,19 @@ app.use(express.json());
 //use ejs
 app.set('view engine', 'ejs');
 //tell express where to find the views
-app.set('views', 'views');
+app.set('views', __dirname + '/views');
 //define a static folder
 app.use(express.static('public'));
 
 // Define routes
 const router = require('./routes/router.js');
 app.use('/', router);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 // Export the Express app
 module.exports = app;
